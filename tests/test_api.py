16b6f288d1c445256f57dd501514e864d12872d6
eugene <eugene>
2023-10-31 22:15:51 +0300

    test

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,4 +5,4 @@ def test_api():
     url = 'http://app:5000/'
     response = requests.get(url)
     assert response.status_code == 200
-    assert 'Hello, my frend!' in response.text
+    assert 'Hello, my frend' in response.text
